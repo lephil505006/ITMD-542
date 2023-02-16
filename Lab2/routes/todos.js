24370d705f9873efe5203beeb01f768be4774e85
Philip Le <lephil505006@gmail.com>
2023-02-16 13:46:37 -0600

    Practicing with ids and putting them out

--- Lab2/routes/todos.js
+++ Lab2/routes/todos.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+let data = [
+  { text: 'This is text 1', id: '760f5b3b-09d8-48cb-80c1-63469bf13bb5' },
+  { text: 'This is text 2', id: '12e4350f-72d3-4996-90d8-cef6c6340c53' }
+];
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.render('todos', { title: 'Lab 2' });
+  res.render('todos', { title: 'Lab 2', todos: data });
 });
 
 module.exports = router;
