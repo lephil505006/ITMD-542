839021c828bd73e7bcad14a45abcba79de17caa0
Philip Le <lephil505006@gmail.com>
2023-02-16 17:56:49 -0600

    Making repo

---
+++ Lab2/src/todosMemoryRepository.js
@@ -0,0 +1,10 @@
+const db = new Map();
+
+db.set('760f5b3b-09d8-48cb-80c1-63469bf13bb5', { text: 'This is text 1', id: '760f5b3b-09d8-48cb-80c1-63469bf13bb5' });
+db.set('12e4350f-72d3-4996-90d8-cef6c6340c53', { text: 'This is text 2', id: '12e4350f-72d3-4996-90d8-cef6c6340c53' });
+
+const repo = {
+    findAll: () => Array.from(db.values()),
+};
+
+module.exports = repo;
