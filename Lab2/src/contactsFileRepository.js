dc01d056e40bc1ba6d9158f94d1fc1e9e0295e25
Philip Le <lephil505006@gmail.com>
2023-02-18 15:52:41 -0600

    removing old key

--- Lab2/src/contactsFileRepository.js
+++ Lab2/src/contactsFileRepository.js
@@ -3,9 +3,6 @@ const fs = require('fs');
 const path = require('path');
 const db = new Map();
 
-//db.set('760f5b3b-09d8-48cb-80c1-63469bf13bb5', { text: 'This is text 1', id: '760f5b3b-09d8-48cb-80c1-63469bf13bb5' });
-//db.set('12e4350f-72d3-4996-90d8-cef6c6340c53', { text: 'This is text 2', id: '12e4350f-72d3-4996-90d8-cef6c6340c53' });
-
 const loadData = () => {
     const jsonData = fs.readFileSync(path.join(__dirname, '../data/contacts.json'));
     const contactsArray = JSON.parse(jsonData);
