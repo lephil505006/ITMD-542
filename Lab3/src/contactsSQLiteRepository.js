d1385fcc7361891f28e27c1d0a0a4d71891da451
Philip Le <lephil505006@gmail.com>
2023-02-28 09:57:24 -0600

    putting base sqlite code

--- Lab3/src/contactsSQLiteRepository.js
+++ Lab3/src/contactsSQLiteRepository.js
@@ -1,45 +1,52 @@
 const crypto = require('crypto');
 const fs = require('fs');
 const path = require('path');
-const db = new Map();
+const betterSqlite3 = require('better-sqlite3');
 
-const loadData = () => {
-    const jsonData = fs.readFileSync(path.join(__dirname, '../data/contacts.json'));
-    const contactsArray = JSON.parse(jsonData);
-    contactsArray.forEach(element => {
-        db.set(element[0], element[1]);
-    });
-};
+const db = new betterSqlite3(path.join(__dirname, '../data/contacts.sqlite'), { verbose: console.log });
 
-const saveData = () => {
-    const stringifyData = JSON.stringify(Array.from(db));
-    fs.writeFileSync(path.join(__dirname, '../data/contacts.json'), stringifyData);
-};
+const createStmt = db.prepare("CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)");
+createStmt.run();
+
+//const loadData = () => {
+//const jsonData = fs.readFileSync(path.join(__dirname, '../data/contacts.json'));
+//const contactsArray = JSON.parse(jsonData);
+//contactsArray.forEach(element => {
+// db.set(element[0], element[1]);
+// });
+//};
+
+//const saveData = () => {
+//const stringifyData = JSON.stringify(Array.from(db));
+//fs.writeFileSync(path.join(__dirname, '../data/contacts.json'), stringifyData);
+//};
 
 const repo = {
-    findAll: () => Array.from(db.values()),
-    findById: (uuid) => db.get(uuid),
+    findAll: () => {
+        return [];
+    },
+    findById: (uuid) => {
+        return {};
+    },
     create: (contact1, contact2, contact3) => {
         //(code for time but editing caused errors) const date = fs.statSync(path.join(__dirname, '../data/contacts.json'));
         const newContact = {
-            id: crypto.randomUUID(),
-            firstName: contact1.text,
-            lastName: contact2.text,
-            email: contact3.text,
+            //id: crypto.randomUUID(),
+            //firstName: contact1.text,
+            //lastName: contact2.text,
+            //email: contact3.text,
         };
-        db.set(newContact.id, newContact);
-        saveData();
+        //db.set(newContact.id, newContact);
+        //saveData();
     },
     deleteById: (uuid) => {
-        db.delete(uuid);
-        saveData();
+        //db.delete(uuid);
+        //saveData();
     },
     update: (contact) => {
-        db.set(contact.id, contact);
-        saveData();
+        //db.set(contact.id, contact);
+        //saveData();
     },
 };
-
-loadData();
 
 module.exports = repo;
