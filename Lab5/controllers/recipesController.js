26a946fd68709965856223f3c6280be05b3e1488
Philip Le <lephil505006@gmail.com>
2023-04-28 21:34:08 -0500

    Editing titles for qulaity of life

--- Lab5/controllers/recipesController.js
+++ Lab5/controllers/recipesController.js
@@ -4,12 +4,12 @@ const Recipe = require('../src/Recipe');
 /* GET users listing. */
 exports.recipes_list = async function (req, res, next) {
     const data = await recipesRepo.findAll();
-    res.render('recipes', { title: 'recipe Database', recipes: data });
+    res.render('recipes', { title: 'Recipe Database', recipes: data });
 };
 
 /* GET create recipe form */
 exports.recipes_create_get = async function (req, res, next) {
-    res.render('recipes_add', { title: 'Add a recipes' });
+    res.render('recipes_add', { title: 'Add a recipe' });
 };
 
 /* POST create recipe form */
@@ -31,7 +31,7 @@ exports.recipes_create_post = async function (req, res, next) {
 exports.recipes_detail = async function (req, res, next) {
     const recipe = await recipesRepo.findById(req.params.uuid);
     if (recipe) {
-        res.render('recipe', { title: 'Your Todo', recipe: recipe })
+        res.render('recipe', { title: 'Recipe', recipe: recipe })
     }
     else {
         res.redirect('/recipes');
@@ -42,7 +42,7 @@ exports.recipes_detail = async function (req, res, next) {
 /* GET delete recipe form */
 exports.recipes_delete_get = async function (req, res, next) {
     const recipe = await recipesRepo.findById(req.params.uuid);
-    res.render('recipes_delete', { title: 'Delete recipes', recipe: recipe });
+    res.render('recipes_delete', { title: 'Delete recipe', recipe: recipe });
 };
 
 /* POST delete recipe form  */
@@ -54,14 +54,14 @@ exports.recipes_delete_post = async function (req, res, next) {
 /* GET edit recipe form */
 exports.recipes_edit_get = async function (req, res, next) {
     const recipe = await recipesRepo.findById(req.params.uuid);
-    res.render('recipes_edit', { title: 'Edit recipes', recipe: recipe });
+    res.render('recipes_edit', { title: 'Edit recipe', recipe: recipe });
 };
 
 /* POST edit recipe form */
 exports.recipes_edit_post = async function (req, res, next) {
     if (req.body.recipeDish.trim() === '') {
         const recipe = await recipesRepo.findById(req.params.uuid);
-        res.render('recipes_edit', { title: 'Edit Todo', msg: 'Todo text can not be empty!', recipe: recipe })
+        res.render('recipes_edit', { title: 'Edit recipe', msg: 'Recipe text can not be empty!', recipe: recipe })
     }
     else {
         const updatedRecipe = new Recipe(req.params.uuid, req.body.recipeDish.trim(), req.body.recipeOccasion.trim(), req.body.recipeServing.trim(), req.body.recipeDescription.trim(), req.body.recipeInstruction.trim());
